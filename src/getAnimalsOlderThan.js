4c1613a164ec476260cec0ab8ccae51ed84434a8
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-09 15:11:52 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
-};
+const getAnimalsOlderThan = (animal, age) =>
+  data.species
+    .find(({ name }) => name === animal)
+    .residents.every((zooAnimal) => zooAnimal.age >= age);
 
 module.exports = getAnimalsOlderThan;
