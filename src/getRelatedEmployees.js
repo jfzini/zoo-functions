f41b3b73b598f88099aa10a7394961736a2a0374
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-09 16:38:43 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) =>
+  data.employees.some((employee) => employee.managers.some((managerId) => managerId === id));
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees
+    .filter((employee) => employee.managers
+      .some((manager) => manager === managerId))
+    .map((element) => `${element.firstName} ${element.lastName}`);
 };
 
 module.exports = { isManager, getRelatedEmployees };
