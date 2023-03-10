2c1c05fbf22fb7d6b968c0167c08c428ede63e50
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-10 09:27:52 -0300

    explorando set como possibilidade

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
+const daysList = 
+  Object.keys(new Set(data.species.map(({ availability }) => availability)
+    .reduce((a, b) => a.concat(b))));
+console.log(daysList);
+const validateTarget = (scheduleTarget) => {
+  const animalsArray = data.species.map(({name}) => name);
+  const daysSet = 
+  new Set(data.species.map(({ availability }) => availability)
+    .reduce((a, b) => a.concat(b)));
+  console.log(animalsList);
+  if (!scheduleTarget || ) {
+    
+  }
+}
 
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  if (!scheduleTarget) {
+    
+  }
+  return data.species.find(({ name }) => name === scheduleTarget).availability;
 };
+
+console.log(getSchedule('tigers'));
 
 module.exports = getSchedule;
