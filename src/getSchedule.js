8b1594aca3c6b4261624500cb1f4c67f6838cf16
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-10 09:33:05 -0300

    explorando set como possibilidade/2

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,17 +1,14 @@
 const data = require('../data/zoo_data');
-const daysList = 
-  Object.keys(new Set(data.species.map(({ availability }) => availability)
-    .reduce((a, b) => a.concat(b))));
-console.log(daysList);
-const validateTarget = (scheduleTarget) => {
+
+const validateTarget = (target) => {
   const animalsArray = data.species.map(({name}) => name);
-  const daysSet = 
-  new Set(data.species.map(({ availability }) => availability)
-    .reduce((a, b) => a.concat(b)));
-  console.log(animalsList);
-  if (!scheduleTarget || ) {
-    
+  const daysSet = //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set set retira itens repetidos;
+    new Set(data.species.map(({ availability }) => availability)
+      .reduce((a, b) => a.concat(b)));
+  if (!target || !animalsArray.includes(target) || !daysSet.has(target)) {
+    return false;
   }
+  return true;
 }
 
 const getSchedule = (scheduleTarget) => {
