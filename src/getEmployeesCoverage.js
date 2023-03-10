c5ac2dd75249023736b5b7cd89973f7d632d5aae
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-10 13:29:55 -0300

    melhorando legibilidade requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -3,8 +3,21 @@ const data = require('../data/zoo_data');
 const findEmployee = (name, parmID) => {
   const employeeByName = data.employees.find((employee) => Object.values(employee).includes(name));
   const employeeByID = data.employees.find((employee) => Object.values(employee).includes(parmID));
-
   return employeeByName || employeeByID;
+};
+
+const getSpecificEmployee = (objParam) => {
+  const { name, id: parmID } = objParam;
+  const foundEmployee = findEmployee(name, parmID);
+  const { id, firstName, lastName, responsibleFor } = foundEmployee;
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: responsibleFor
+      .map((element) => data.species.find((animal) => animal.id === element).name),
+    locations: responsibleFor
+      .map((element) => data.species.find((animal) => animal.id === element).location),
+  };
 };
 
 const getAllEmployess = () => data.employees.map(({ id, firstName, lastName, responsibleFor }) => ({
@@ -20,20 +33,11 @@ const getEmployeesCoverage = (objParam) => {
   if (!objParam) {
     return getAllEmployess();
   }
-
   try {
-    const { name, id: parmID } = objParam;
-    const foundEmployee = findEmployee(name, parmID);
-    const { id, firstName, lastName, responsibleFor } = foundEmployee;
-    return {
-      id,
-      fullName: `${firstName} ${lastName}`,
-      species: responsibleFor
-        .map((element) => data.species.find((animal) => animal.id === element).name),
-      locations: responsibleFor
-        .map((element) => data.species.find((animal) => animal.id === element).location),
-    };
-  } catch (error) { throw new Error('Informações inválidas'); }
+    return getSpecificEmployee(objParam);
+  } catch (error) {
+    throw new Error('Informações inválidas');
+  }
 };
 
 module.exports = getEmployeesCoverage;
