de8b4150194d6654dfc0c66aa979c410e846c2f3
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-10 10:52:48 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+const getOldestFromFirstSpecies = (employeeID) => {
+  const firstAnimalID = data.employees.find(({ id }) => id === employeeID).responsibleFor[0];
+  const oldestAnimal = data.species.find(({ id }) => id === firstAnimalID).residents
+    .sort((a, b) => b.age - a.age)[0];
+  const { name, sex, age } = oldestAnimal;
+  return [name, sex, age];
 };
 
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
