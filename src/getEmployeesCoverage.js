7d31f86c9b4ffc8d71cea939b436a7c9cbe3798c
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-10 13:03:41 -0300

    trabalho requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const findEmployee = (name, parmID) => {
+    const employeeByName = data.employees.find((employee) => Object.values(employee).includes(name));
+    const employeeByID = data.employees.find((employee) => Object.values(employee).includes(parmID));
+  
+  return employeeByName || employeeByID;
+}
+
+const getEmployeesCoverage = (objParam) => {
+  const { name, id: parmID } = objParam;
+  try {
+    const foundEmployee = findEmployee(name, parmID);
+    const { id, firstName, lastName, responsibleFor } = foundEmployee;
+    return {
+      id,
+      fullName: `${firstName} ${lastName}`,
+      species: responsibleFor.map((element) => data.species.find(({ id }) => id === element).name),
+      locations: responsibleFor.map((element) => data.species.find(({ id }) => id === element).location)
+    }
+  }
+  catch {
+    throw new Error('Informações inválidas');
+  }
+  
 };
+
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
