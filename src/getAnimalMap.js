b3a72b7acd8234be069a42bc05abcd0cf4f276aa
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-10 19:11:37 -0300

    desenvolvendo 14 ainda

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,14 +1,20 @@
 const data = require('../data/zoo_data');
 
-const getNamesArray = (speciesLocated) => {
-  const teste = data.species.find((animal) => animal.name === speciesLocated).residents.map(({ name }) => name);
+const getNamesArray = (speciesLocated, sorted) => {
+  let teste = data.species.find((animal) => animal.name === speciesLocated).residents.map(({ name }) => name);
+  if (sorted) {
+    teste = data.species.find((animal) => animal.name === speciesLocated).residents.map(({ name }) => name).sort();
+  }
   return teste;
-}
+};
 
-const getNamesBySex = (speciesLocated, speciesSex) => {
-  const namesBySex = data.species.find((animal) => animal.name === speciesLocated).residents.filter(({ sex }) => sex === speciesSex).map(({ name }) => name);
+const getNamesBySex = (speciesLocated, speciesSex, sorted) => {
+  let namesBySex = data.species.find((animal) => animal.name === speciesLocated).residents.filter(({ sex }) => sex === speciesSex).map(({ name }) => name);
+  if (sorted) {
+    namesBySex = data.species.find((animal) => animal.name === speciesLocated).residents.filter(({ sex }) => sex === speciesSex).map(({ name }) => name).sort();
+  }
   return namesBySex;
-}
+};
 
 const getAnimalMap = (options) => data.species.reduce((acc, { location }) => {
   const speciesLocation = data.species
@@ -20,7 +26,7 @@ const getAnimalMap = (options) => data.species.reduce((acc, { location }) => {
   }
 
   acc[location] = speciesLocation
-    .map((speciesLocated) => ({ [speciesLocated]: getNamesArray(speciesLocated) }));
+    .map((speciesLocated) => ({ [speciesLocated]: getNamesArray(speciesLocated, options.sorted || false) }));
 
   // if (options && options.sorted) {
   //   acc[location] = speciesLocation
@@ -29,9 +35,8 @@ const getAnimalMap = (options) => data.species.reduce((acc, { location }) => {
 
   if (options && options.sex) {
     acc[location] = speciesLocation
-      .map((speciesLocated) => ({ [speciesLocated]: getNamesBySex(speciesLocated, options.sex) }));
+      .map((speciesLocated) => ({ [speciesLocated]: getNamesBySex(speciesLocated, options.sex, options.sorted || false) }));
   }
-
   // if (options && options.sorted && options.sex) {
   //   acc[location] = speciesLocation
   //     .map((speciesLocated) => ({ [speciesLocated]: data.species.find((animal) => animal.name === speciesLocated).residents.filter(({ sex }) => sex === options.sex).map(({ name }) => name).sort() }));
@@ -40,6 +45,13 @@ const getAnimalMap = (options) => data.species.reduce((acc, { location }) => {
   return acc;
 }, {});
 
-console.log((getAnimalMap({includeNames: true, sex: 'male'})));
+console.log((getAnimalMap({ includeNames: true, sex: 'male' })));
 
 module.exports = getAnimalMap;
+
+const teste = (options) => data.species.reduce((acc, { location }) => {
+  acc[location] = speciesLocation
+    .map((speciesLocated) => ({ [speciesLocated]: data.species.reduce((a, b) => {
+
+    }, []) }));
+}, {});
