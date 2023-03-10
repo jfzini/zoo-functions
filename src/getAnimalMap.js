7b78be7c068103a733a0f4d61bf407e29562976a
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-10 19:36:24 -0300

    14 dilacerado em outras funções

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,57 +1,61 @@
 const data = require('../data/zoo_data');
 
 const getNamesArray = (speciesLocated, sorted) => {
-  let teste = data.species.find((animal) => animal.name === speciesLocated).residents.map(({ name }) => name);
+  let teste = data.species
+    .find((animal) => animal.name === speciesLocated).residents
+    .map(({ name }) => name);
   if (sorted) {
-    teste = data.species.find((animal) => animal.name === speciesLocated).residents.map(({ name }) => name).sort();
+    teste = data.species
+      .find((animal) => animal.name === speciesLocated).residents
+      .map(({ name }) => name).sort();
   }
   return teste;
 };
 
 const getNamesBySex = (speciesLocated, speciesSex, sorted) => {
-  let namesBySex = data.species.find((animal) => animal.name === speciesLocated).residents.filter(({ sex }) => sex === speciesSex).map(({ name }) => name);
+  let namesBySex = data.species
+    .find((animal) => animal.name === speciesLocated).residents
+    .filter(({ sex }) => sex === speciesSex)
+    .map(({ name }) => name);
   if (sorted) {
-    namesBySex = data.species.find((animal) => animal.name === speciesLocated).residents.filter(({ sex }) => sex === speciesSex).map(({ name }) => name).sort();
+    namesBySex = data.species
+      .find((animal) => animal.name === speciesLocated).residents
+      .filter(({ sex }) => sex === speciesSex)
+      .map(({ name }) => name).sort();
   }
   return namesBySex;
 };
 
-const getAnimalMap = (options) => data.species.reduce((acc, { location }) => {
+const withoutNames = () => data.species.reduce((acc, { location }) => {
   const speciesLocation = data.species
     .filter((animalSpecies) => animalSpecies.location === location)
     .map((element) => element.name);
-  if (!options || !options.includeNames) {
-    acc[location] = speciesLocation;
-    return acc;
-  }
-
-  acc[location] = speciesLocation
-    .map((speciesLocated) => ({ [speciesLocated]: getNamesArray(speciesLocated, options.sorted || false) }));
-
-  // if (options && options.sorted) {
-  //   acc[location] = speciesLocation
-  //     .map((speciesLocated) => ({ [speciesLocated]: data.species.find((animal) => animal.name === speciesLocated).residents.map(({ name }) => name).sort() }));
-  // }
-
-  if (options && options.sex) {
-    acc[location] = speciesLocation
-      .map((speciesLocated) => ({ [speciesLocated]: getNamesBySex(speciesLocated, options.sex, options.sorted || false) }));
-  }
-  // if (options && options.sorted && options.sex) {
-  //   acc[location] = speciesLocation
-  //     .map((speciesLocated) => ({ [speciesLocated]: data.species.find((animal) => animal.name === speciesLocated).residents.filter(({ sex }) => sex === options.sex).map(({ name }) => name).sort() }));
-  // }
-
+  acc[location] = speciesLocation;
   return acc;
 }, {});
 
-console.log((getAnimalMap({ includeNames: true, sex: 'male' })));
+const withNames = (options) => data.species.reduce((acc, { location }) => {
+  const speciesLocation = data.species
+    .filter((animalSpecies) => animalSpecies.location === location)
+    .map((element) => element.name);
+
+  acc[location] = speciesLocation
+    .map((speciesLocated) => ({ [speciesLocated]:
+        getNamesArray(speciesLocated, options.sorted || false) }));
+
+  if (options && options.sex) {
+    acc[location] = speciesLocation
+      .map((speciesLocated) => ({ [speciesLocated]:
+          getNamesBySex(speciesLocated, options.sex, options.sorted || false) }));
+  }
+  return acc;
+}, {});
+
+const getAnimalMap = (options) => {
+  if (!options || !options.includeNames) {
+    return withoutNames();
+  }
+  return withNames(options);
+};
 
 module.exports = getAnimalMap;
-
-const teste = (options) => data.species.reduce((acc, { location }) => {
-  acc[location] = speciesLocation
-    .map((speciesLocated) => ({ [speciesLocated]: data.species.reduce((a, b) => {
-
-    }, []) }));
-}, {});
