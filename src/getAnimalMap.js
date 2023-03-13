029d4b950432eacf8519f2c1eca349fd705affca
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-13 18:34:08 -0300

    requisito 14 com declaração de objeto inicial com chaves pre-definidas

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -58,18 +58,25 @@ const data = require('../data/zoo_data');
 //   return withNames(options);
 // };
 
-const getAnimalMap = (options) = 
-  data.species.reduce((acc, { location }) => {
-      acc[location] = speciesLocation
-        .map((speciesLocated) => ({ [speciesLocated]:
-            getNamesArray(speciesLocated, options.sorted || false) }));
-    
-      if (options && options.sex) {
-        acc[location] = speciesLocation
-          .map((speciesLocated) => ({ [speciesLocated]:
-              getNamesBySex(speciesLocated, options.sex, options.sorted || false) }));
-      }
-      return acc;
-    }, { NE: [], NW: [], SE: [], SW: [] })
+const generateAnimalObj = (paramName, { sex = false, sorted = false }) => {
+  const animalObj = {};
+  let animalNames = data.species.find(({ name: curName }) => paramName === curName).residents
+    .map(({ name }) => name);
+  if (sex) {
+    animalNames = data.species.find(({ name: curName }) => paramName === curName).residents
+      .filter(({ sex: curSex }) => sex === curSex)
+      .map(({ name }) => name);
+  }
+  if (sorted) animalObj[paramName] = animalNames.sort();
+  else animalObj[paramName] = animalNames;
+  return animalObj;
+};
+
+const getAnimalMap = (options) =>
+  data.species.reduce((acc, { name, location }) => {
+    if (!options || !options.includeNames) acc[location].push(name);
+    else acc[location].push(generateAnimalObj(name, options));
+    return acc;
+  }, { NE: [], NW: [], SE: [], SW: [] });
 
 module.exports = getAnimalMap;
